31c4f94f668d3dc29e8ec240f5ae8e61e0cdec45
Calabaza <calalinux@gmail.com>
2024-04-17 10:32:17 -0400

    Added - Exercise 4.20: Numeric Type Conversion

--- main.go
+++ main.go
@@ -2,65 +2,28 @@ package main
 
 import (
     "fmt"
+    "math"
 )
 
-type name string
-
-type location struct {
-    x int
-    y int
+func convert() string{
+    var i8 int8 = math.MaxInt8
+    i := 128
+    f64 := 3.14
+     m := fmt.Sprintf("int8  = %v > int64  = %v\n", i8, int64(i8))
+     // This will cause an overflow to int8's minimum size:
+     m += fmt.Sprintf("int   = %v > int8   = %v\n", i, int8(i))
+     m += fmt.Sprintf("int8  = %v > float64 = %v\n", i8, float64(i8))
+     m += fmt.Sprintf("float64 = %v > int   = %v\n", f64, int(f64))
+     return m
 }
-
-type size struct {
-    width int
-    height int
-}
-
-type dot struct {
-    name
-    location
-    size
-}
-func getDots() []dot {
-    var dot1 dot
-    dot2 := dot{}
-    dot2.name = "A"
-    dot2.x = 5
-    dot2.y = 6
-    dot2.width = 10
-    dot2.height = 20
-
-    dot3 := dot{
-        name: "B",
-        location: location{
-            x: 13,
-            y: 27,
-        },
-        size: size{
-            width: 5,
-            height: 7,
-        },
-    }  
-    dot4 := dot{}
-    dot4.name = "C"
-    dot4.location.x = 101
-    dot4.location.y = 209
-    dot4.size.width = 87
-    dot4.size.height = 43
-    return []dot{dot1, dot2, dot3, dot4}    
-}
-
 
 func main() {
     // Title:
-    fmt.Println ("Exercise 4.19: Struct Embedding and Initialization")
-    fmt.Println("Pag. 155 - PDF:188\n\r")
+    fmt.Println ("Exercise 4.20: Numeric Type Conversion")
+    fmt.Println("Pag. 158 - PDF:191\n\r")
     // Possible states: Started, Finished
     fmt.Println("Status: Finished\n\r")
     
-    dots := getDots()
-    for i := 0; i < len(dots); i++ {
-        fmt.Printf("dot%v: %#v\n", i+1, dots[i])
-    }
+    fmt.Print(convert())
   
 } // End main()
